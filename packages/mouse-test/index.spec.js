475a44dd1bc4126dc0ceae1bee8e8ed5ec637451
Simon Hofmann <mail@simon-hofmann.org>
2019-02-18 14:57:30 +0100

    Updated mouse example

--- packages/mouse-test/index.spec.js
+++ packages/mouse-test/index.spec.js
@@ -1,19 +1,16 @@
 "use strict";
 
-const native = require("@nut-tree/nut-js");
+const { mouse, movement } = require("@nut-tree/nut-js");
 
-const square = async (control) => {
-    await control.mouse.move(control.movement.right(500));
-    await control.mouse.move(control.movement.down(500));
-    await control.mouse.move(control.movement.left(500));
-    await control.mouse.move(control.movement.up(500));
+const square = async () => {
+  await mouse.move(movement.right(500));
+  await mouse.move(movement.down(500));
+  await mouse.move(movement.left(500));
+  await mouse.move(movement.up(500));
 };
 
 describe("Basic mouse test", () => {
     it("should move the mouse in square shape", async () => {
-        const config = new native.Config();
-        const control = new native.Controller(config);
-
-        await square(control);
+        await square();
     });
 });
