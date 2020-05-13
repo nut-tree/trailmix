bf0d57646a3beab1f2ad1fcb14657793bd927000
Simon Hofmann <mail@simon-hofmann.org>
2020-05-13 23:21:04 +0200

    Updated screen samples, added README.md

--- packages/screen-test/index.spec.js
+++ packages/screen-test/index.spec.js
@@ -1,12 +1,56 @@
 "use strict";
 
-const { screen, mouse, centerOf, straightTo } = require("@nut-tree/nut-js");
+const { screen, Region } = require("@nut-tree/nut-js");
 
-describe("Basic mouse test", () => {
-    it("should move the mouse in square shape", async () => {
-        jest.setTimeout(10000);
-        screen.config.resourceDirectory = "../../e2e/assets";
+describe("Screen test", () => {
+    describe("dimensions", () => {
+        it("should log screen height", async () => {
+            console.log(await screen.height());
+        });
 
-        await mouse.move(straightTo(centerOf(screen.find("mouse.png"))));
+        it("should log screen width", async () => {
+            console.log(await screen.width());
+        });
     });
+
+    describe("highlight", () => {
+        it("should highlight a given Region", async () => {
+            await screen.highlight(new Region(100, 100, 500, 500));
+        })
+    })
+
+    describe("find", () => {
+        it("should log region of match", async () => {
+            jest.setTimeout(10000);
+            screen.config.resourceDirectory = "../../e2e/assets";
+
+            console.log(await screen.find("mouse.png"));
+        });
+
+        it("should report region with highest match when no match with sufficient confidence is found", async () => {
+            jest.setTimeout(10000);
+            screen.config.resourceDirectory = "../../e2e/assets";
+
+            console.log(await screen.find("no_match.png"));
+        });
+    })
+
+    describe("waitFor", () => {
+        it("should wait for a match", async () => {
+            jest.setTimeout(10000);
+            screen.config.resourceDirectory = "../../e2e/assets";
+
+            await screen.waitFor("mouse.png", 2500);
+        });
+    })
+
+    describe("on hook", () => {
+        it("should trigger hook on match", async () => {
+            screen.config.resourceDirectory = "../../e2e/assets";
+            screen.config.confidence = 0.1;
+
+            screen.on("mouse.png", target => console.log(`Match found! ${JSON.stringify(target)}`));
+            await screen.find("mouse.png");
+        });
+    })
 });
