5940147bb1768bbcb67895ee2234331a83e3d641
Simon Hofmann <mail@simon-hofmann.org>
2021-04-15 11:27:05 +0200

    Disabled Electron contextIsolation

--- packages/electron-sample/main.js
+++ packages/electron-sample/main.js
@@ -8,6 +8,7 @@ function createWindow() {
         height: 600,
         webPreferences: {
             nodeIntegration: true,
+            contextIsolation: false,
             preload: path.join(__dirname, 'preload.js')
         }
     })
